64d9613aadf24827bd7a4278510e43a5a4f80373
newtechie7 <newtechie7@gmail.com>
2020-02-15 14:36:33 +0530

    core-java | first core java program implemented using note pad and eclipse ide, code explanation also added

---
+++ first-core-java-program/code/notepade-code/PrintTextApplication.java
@@ -0,0 +1,11 @@
+package com.myowncompany.myapp.text.print;
+
+public class PrintTextApplication {
+	
+	public static void main(String[] strings) {
+		
+		System.out.println("This is my first java program");
+		
+	}
+	
+}
